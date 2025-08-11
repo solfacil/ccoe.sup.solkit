b49e647ee999eecb965b9fa79e00af0dc01df236
LuizFerraresi <luiz.ferraresi@solfacil.com.br>
2025-08-11 14:55:04 -0300

    add dlq and retry attempt suffixes

--- solkit/broker/constants.py
+++ solkit/broker/constants.py
@@ -1,9 +1,12 @@
 from enum import StrEnum
 
+BROKER_HEARTBEAT_PER_SESSION = 4
+BROKER_RETRY_SUFFIX = '-RETRY-'
+BROKER_DEAD_LETTER_QUEUE_SUFFIX = '-DLQ'
+
 
 class BrokerKafkaAcks(StrEnum):
     """Valid values for Kafka Producer ACKS."""
-    
     ALL = "all"
     ZERO = "0"
     ONE = "1"
