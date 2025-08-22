98cfae59c423fc7dd2e587d1b3c59cee14ccbd45
LuizFerraresi <luiz.ferraresi@solfacil.com.br>
2025-08-22 15:37:27 -0300

    add docstrings and typing for cache abc

--- solkit/cache/abstracts.py
+++ solkit/cache/abstracts.py
@@ -1,19 +1,35 @@
 from abc import ABC, abstractmethod
 
+from redis.asyncio.client import Redis
+from redis.asyncio.cluster import RedisCluster
+
+from .settings import CacheRedisSettings
+
 
 class CacheAdapterAbstract(ABC):
-    def __init__(self, settings) -> None:
-        ...
-        
-    def config(self):
-        ...
-
-    def connect(self) -> None:
-        ...
-        
-    def disconnect(self) -> None:
-        ...
+    """Cache adapter abstract class."""
     
     @abstractmethod
-    def get_session(self):
-        ...
+    @classmethod
+    def config(cls) -> None:
+        """Configure the cache adapter."""
+        raise NotImplementedError()
+    
+    def __init__(self, settings: CacheRedisSettings) -> None:
+        """Initialize the cache adapter."""
+        self._settings = settings
+
+    @abstractmethod
+    async def connect(self) -> None:
+        """Connect to the cache."""
+        raise NotImplementedError()
+    
+    @abstractmethod
+    async def disconnect(self) -> None:
+        """Disconnect from the cache."""
+        raise NotImplementedError()
+    
+    @abstractmethod
+    async def get_session(self) -> Redis | RedisCluster:
+        """Get a session from the cache."""
+        raise NotImplementedError()
