4a66a8442c15a6dc3f39e49112ac33828662f3ee
LuizFerraresi <luiz.ferraresi@solfacil.com.br>
2025-08-25 17:47:52 -0300

    update common repository name

--- solkit/common/protocols/repository.py
+++ solkit/common/protocols/repository.py
@@ -2,7 +2,7 @@ from typing import Protocol, runtime_checkable
 
 
 @runtime_checkable
-class BaseRepository(Protocol):
+class BaseRepositoryProtocol(Protocol):
     """Base repository protocol."""
     
     async def healthcheck(self) -> tuple[bool, str | None]:
