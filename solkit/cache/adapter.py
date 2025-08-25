ea9bede66ab4d5b84c37d656d0e36fa3354d1edf
LuizFerraresi <luiz.ferraresi@solfacil.com.br>
2025-08-25 17:29:36 -0300

    fix lint

--- solkit/cache/adapter.py
+++ solkit/cache/adapter.py
@@ -1,6 +1,7 @@
+import logging
+from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 from typing import Any
-import logging
 
 from redis.asyncio.client import Redis
 from redis.asyncio.cluster import RedisCluster
@@ -9,43 +10,43 @@ from redis.asyncio.retry import Retry
 from redis.backoff import ExponentialBackoff
 
 from .constants import CacheDeploymentMode
-from .settings import CacheRedisClusterSettings, CacheModeSettings, CacheRedisSingleNodeSettings
+from .settings import CacheModeSettings, CacheRedisClusterSettings, CacheRedisSingleNodeSettings
 
 logger = logging.getLogger(__name__)
 
 
 class CacheRedisAdapter:
     """Cache redis adapter."""
-    
+
     @classmethod
-    def single_node_config(cls) -> "CacheRedisAdapter":
+    def single_node_config(cls) -> 'CacheRedisAdapter':
         """Create a single node cache adapter."""
         settings = CacheRedisSingleNodeSettings()
         return cls(settings)
-    
+
     @classmethod
-    def cluster_config(cls) -> "CacheRedisAdapter":
+    def cluster_config(cls) -> 'CacheRedisAdapter':
         """Create a cluster cache adapter."""
         settings = CacheRedisClusterSettings()
         return cls(settings)
-    
+
     @classmethod
-    def config(cls) -> "CacheRedisAdapter":
+    def config(cls) -> 'CacheRedisAdapter':
         """Create a cache adapter based on the deployment mode."""
         cache_mode_settings = CacheModeSettings()
         return (
             cls.cluster_config()
-            if cache_mode_settings.deployment_mode == CacheDeploymentMode.CLUSTER 
+            if cache_mode_settings.deployment_mode == CacheDeploymentMode.CLUSTER
             else cls.single_node_config()
         )
-        
+
     def __init__(self, settings: CacheRedisClusterSettings | CacheRedisSingleNodeSettings) -> None:
         """Initialize the cache adapter."""
         self._connection_pool: ConnectionPool
         self._single_node_connection: Redis
         self._cluster_connection: RedisCluster
         self._settings = settings
-        
+
     @property
     def __retry_config(self) -> dict[str, Any]:
         retry_config = {
@@ -53,7 +54,7 @@ class CacheRedisAdapter:
             'retry_on_error': [ConnectionError, TimeoutError],
         }
         return retry_config
-    
+
     @property
     def __common_config(self) -> dict[str, Any]:
         common_config = {
@@ -67,7 +68,7 @@ class CacheRedisAdapter:
             'health_check_interval': self._settings.health_check_interval,
         }
         return common_config
-    
+
     @property
     def __cluster_config(self) -> dict[str, Any]:
         cluster_config = {
@@ -77,7 +78,7 @@ class CacheRedisAdapter:
             'require_full_coverage': self._settings.require_full_coverage,  # type: ignore
         }
         return cluster_config
-    
+
     @property
     def __single_node_config(self) -> dict[str, Any]:
         single_node_config = {
@@ -85,32 +86,32 @@ class CacheRedisAdapter:
             'retry_on_timeout': self._settings.retry_on_timeout,  # type: ignore
         }
         return single_node_config
-    
+
     def __create_cluster_connection(self) -> None:
         self._cluster_connection = RedisCluster(**self.__cluster_config)
-        
+
     def __create_single_node_connection(self) -> None:
         self._connection_pool = ConnectionPool(**self.__single_node_config)
         self._single_node_connection = Redis(connection_pool=self._connection_pool)
-         
+
     async def connect(self) -> None:
         """Connect to the cache."""
-        logger.info(f"[ADAPTER][CACHE][CONNECTION URI: {self._settings.build_uri}]")
-        logger.info(f"[ADAPTER][CACHE][CONNECTION MODE: {self._settings.deployment_mode.value.upper()}]")
+        logger.info(f'[ADAPTER][CACHE][CONNECTION URI: {self._settings.build_uri}]')
+        logger.info(f'[ADAPTER][CACHE][CONNECTION MODE: {self._settings.deployment_mode.value.upper()}]')
         if self._settings.deployment_mode == CacheDeploymentMode.CLUSTER:
             self.__create_cluster_connection()
-            logger.info(f"[ADAPTER][CACHE][CONNECTION ACTIVE: {await self._cluster_connection.ping()}]")
-            logger.info(f"[ADAPTER][CACHE][CLUSTER NODES: {self._cluster_connection.get_nodes()}]")
+            logger.info(f'[ADAPTER][CACHE][CONNECTION ACTIVE: {await self._cluster_connection.ping()}]')
+            logger.info(f'[ADAPTER][CACHE][CLUSTER NODES: {self._cluster_connection.get_nodes()}]')
         else:
             self.__create_single_node_connection()
-            logger.info(f"[ADAPTER][CACHE][CONNECTION ACTIVE: {await self._single_node_connection.ping()}]")
-            logger.info(f"[ADAPTER][CACHE][CONNECTION POOL ACTIVE: {self._connection_pool.can_get_connection()}]")
-        
+            logger.info(f'[ADAPTER][CACHE][CONNECTION ACTIVE: {await self._single_node_connection.ping()}]')
+            logger.info(f'[ADAPTER][CACHE][CONNECTION POOL ACTIVE: {self._connection_pool.can_get_connection()}]')
+
     async def __disconnect_cluster_connection(self) -> None:
         if self._cluster_connection:
             await self._cluster_connection.aclose()
             del self._cluster_connection
-            
+
     async def __disconnect_single_node_connection(self) -> None:
         if self._connection_pool:
             await self._connection_pool.aclose()
@@ -123,10 +124,10 @@ class CacheRedisAdapter:
             await self.__disconnect_cluster_connection()
         else:
             await self.__disconnect_single_node_connection()
-        logger.info("[ADAPTER][CACHE][DISCONNECTED]")
-    
+        logger.info('[ADAPTER][CACHE][DISCONNECTED]')
+
     @asynccontextmanager
-    async def get_session(self):
+    async def get_session(self) -> AsyncGenerator[Redis | RedisCluster, None]:
         """Get a session from the cache."""
         redis_connection = (
             self._cluster_connection
--- solkit/database/sql/protocols.py
+++ solkit/database/sql/protocols.py
@@ -1,35 +1,36 @@
-from typing import Any, Protocol, Sequence
+from collections.abc import Sequence
+from typing import Any, Protocol
 
 from sqlalchemy.engine.row import Row
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from .constants import DATABASE_DEFAULT_PAGE_SIZE, DATABASE_DEFAULT_OFFSET, DatabaseSQLSort
+from .constants import DATABASE_DEFAULT_OFFSET, DATABASE_DEFAULT_PAGE_SIZE, DatabaseSQLSort
 from .orm import EntityModel
 
 
 class DatabaseSQLRepositoryProtocol(Protocol):
     """Protocol for the database repository."""
-    
+
     def __init__(self, database_session: AsyncSession, model: type[EntityModel]) -> None:
         """Initialize the database repository."""
         ...
-    
+
     async def commit(self) -> None:
         """Commit the database session."""
         ...
-    
+
     async def get_by_id(self, id: int) -> type[EntityModel] | None:
         """Get a model by id."""
         ...
-    
+
     async def insert(self, data: dict[str, Any]) -> type[EntityModel]:
         """Insert a model."""
         ...
-    
+
     async def update(self, data: dict[str, Any]) -> type[EntityModel] | None:
         """Update a model."""
         ...
-    
+
     async def delete(self, data: type[EntityModel]) -> type[EntityModel] | None:
         """Delete a model."""
         ...
